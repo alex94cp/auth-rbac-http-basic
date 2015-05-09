a16ded55ec1ef4f8b42a898012dbfd3d55ee29d7
alex94puchades <alex94puchades@gmail.com>
2015-05-09 16:28:03 +0200

    Updated authenticate interface

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-function httpBasicCredentialsGiven(req) {
-	return !!req.headers.authorization;
-}
+var rbac = require('auth-rbac');
 
-function httpBasicExtractCredentials(req) {
+function extractCredentials(req) {
 	var authHeader = req.headers.authorization;
+	if (!authHeader)
+		return null;
 	var re = /^Authorization: +Basic +([A-Za-z0-9+]+={0,3})$/i;
 	var data = re.exec(authHeader);
 	if (!data)
-		return {};
+		return null;
 	var buf = new Buffer(data[1], 'base64');
 	var data = buf.toString('utf8');
 	if (!data)
-		return {};
+		return null;
 	var index = data.indexOf(':');
 	if (index == -1)
-		return {};
-	return { user: data.substr(0, index), pass: data.substr(index + 1) };
+		return null;
+	return {
+		user: data.substr(0, index),
+		pass: data.substr(index + 1)
+	};
 }
 
-function httpBasicAskForCredentials(realm) {
+function askForCredentials(realm) {
 	return function(res) {
 		res.status(401)
 		   .header('WWW-Authenticate', 'Basic realm="' + realm + '"')
@@ -26,13 +29,12 @@ function httpBasicAskForCredentials(realm) {
 	};
 }
 
-function httpBasicAuth(auth, realm) {
+function httpBasic(auth, realm) {
 	realm = realm || '';
-	return {
-		credentialsGiven: httpBasicCredentialsGiven,
-		extractCredentials: httpBasicExtractCredentials,
-		askForCredentials: httpBasicAskForCredentials(realm)
-	};
+	return rbac.authenticate(auth, {
+		extractCredentials: extractCredentials,
+		askForCredentials: askForCredentials(realm)
+	});
 }
 
-module.exports = httpBasicAuth;
+module.exports = httpBasic;
